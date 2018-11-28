af311cae93a52d9feb30ed47ac7fbf6dc2013061
Peter Hizalev <peter@hizalev.com>
2018-11-28 10:41:38 -0800

    Update Machine Gun

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule AnalyticsElixir.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:machine_gun, "~> 0.1.1"},
+      {:machine_gun, "~> 0.1.4"},
       {:poison, "~> 1.3 or ~> 2.0 or ~> 3.1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
